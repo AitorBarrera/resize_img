a868f919945373883a3bf78c9887e69dbcf9cd93
Barrera <alumno.138627@ies-azarquiel.es>
2025-01-21 12:35:13 +0100

    DWI_galleria_fotos

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const listaFotos = [
+    {
+        id: 0,
+        url: 'macaco-japones-smartphone-2771257',
+        description: 'Mono nadando con un smartphone'
+    },
+    {
+        id: 1,
+        url: 'pexels-aleksey-kuprikov-1883853-3493731',
+        description: 'Mono madre con mono hijo'
+    },
+    {
+        id: 2,
+        url: 'pexels-andre-mouton-1207875',
+        description: 'Mono reflexionando sobre la vida'
+    },
+    {
+        id: 3,
+        url: 'pexels-arindam-2213575',
+        description: 'Mono gritando'
+    },
+    {
+        id: 4,
+        url: 'pexels-iurii-laimin-78973777-14431127',
+        description: 'Monos besandose'
+    },
+    {
+        id: 5,
+        url: 'pexels-james-boss-1456964-13187517',
+        description: 'Mono sobre tumba'
+    },
+    {
+        id: 6,
+        url: 'pexels-rachel-claire-5865560',
+        description: 'Mono oculto'
+    },
+    {
+        id: 7,
+        url: 'pexels-t6adventures-14598417',
+        description: 'Mono curioso'
+    },
+    {
+        id: 8,
+        url: 'pexels-yankrukov-5479895',
+        description: 'Mono de chill'
+    },
+]
+
+const contenedorFotos = document.querySelector('.contenedorFotos');
+console.log(contenedorFotos);
